b64968ba46ea4e08b292bd1bf080184e6889c87c
Edwin Vargas Ayala <vargas11742@gmail.com>
2023-02-18 22:34:26 -0500

    alertDialog

--- navigation/alert_dialog/lib/main.dart
+++ navigation/alert_dialog/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,15 +30,54 @@ class MyHomePage extends StatelessWidget {
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           ElevatedButton(
             onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Dialog Title"),
+                      content: Text("Body"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Close Dialog"),
+                        )
+                      ],
+                    );
+                  });
             },
             child: Text("Open Dialog"),
             style: ElevatedButton.styleFrom(
                 primary: Colors.blue, textStyle: TextStyle(color: Colors.blue)),
-          )
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return CupertinoAlertDialog(
+                      title: Text("Cupertino Dialog Title"),
+                      content: Text("Body"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Close Dialog"),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: Text("Open Cupertino Dialog"),
+            style: ElevatedButton.styleFrom(
+                primary: Colors.red, textStyle: TextStyle(color: Colors.white)),
+          ),
         ]),
       )),
     );
   }
-
 }
-
